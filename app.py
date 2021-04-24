b85a96685a5eb238d1167a03615765d3e8b6ec32
oluwadolapo <oluwadolapooni@gmail.com>
2021-04-24 11:28:12 +0100

    changes to web interface

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import torch
 from transformers import BartTokenizer, BartForConditionalGeneration
-from transformers import BertTokenizer, BertForSequenceClassification
 from nltk.tokenize import sent_tokenize
 import flask
 from flask import Flask, request, render_template
@@ -15,7 +14,7 @@ class SummarizerModel:
   def __init__(self, output_dir):
     self.model = BartForConditionalGeneration.from_pretrained(output_dir)
     self.tokenizer = BartTokenizer.from_pretrained(output_dir)
-    
+
   def encode(self,question_context):
     encoded = self.tokenizer.encode_plus(question_context, max_length = 512, truncation=True, return_tensors='pt')
     return encoded["input_ids"], encoded["attention_mask"]
@@ -25,24 +24,6 @@ class SummarizerModel:
     summary_ids = self.model.generate(input_ids, attention_mask = attention_mask, num_beams=4, max_length=50)
     summary = [self.tokenizer.decode(id, skip_special_tokens=True, clean_up_tokenization_spaces=True) for id in summary_ids]
     return summary
-
-class AnswerabilityModel:
-  def __init__(self, output_dir):
-    self.tokenizer = BertTokenizer.from_pretrained(output_dir)
-    self.model = BertForSequenceClassification.from_pretrained(output_dir)
-    
-  def encode(self,question_context):
-    encoded = self.tokenizer.encode_plus(question_context, max_length = 100, truncation=True)
-    return encoded["input_ids"], encoded["attention_mask"]
-
-  def predict(self,question_context):
-    input_ids, attention_mask = self.encode(question_context)
-    output = self.model(torch.tensor([input_ids]), attention_mask=torch.tensor([attention_mask]))
-    output = torch.softmax(output[0], dim=1)
-    pred = torch.argmax(output).item()
-    prob = output[0][pred].item()
-    return pred, prob
-
 
 @app.route('/')
 def index():
@@ -60,7 +41,7 @@ def get_answer():
         _set_random_seeds(42)
         q_id = request.json['input_idx']
         q_id = int(q_id)
-        
+
         try:
             df = pd.read_json('/home/oluwadolapo/Datasets/amazonQA_test.json', orient='split')
             question_text = df["question"][q_id]
@@ -73,60 +54,47 @@ def get_answer():
 
             if not question_text.endswith("?"):
                 question_text += "?"
-            
-            bart_model_dir = "/home/oluwadolapo/Experiments/bartQA/model_save1"
-            model = SummarizerModel(bart_model_dir)
-            bart_question_context = "<s> "+question_text+" </s> "+reviews
-            bart_response = model.predict(bart_question_context)[0]
 
-            sent_list = sent_tokenize(reviews)
+            with open('bm25.json', 'r') as json_file:
+                pred_json = json.load(json_file)
+            bm25_response = pred_json['Pred_answer'][q_id].split(".")[0].strip() + "."
 
-            first_response = sent_list[0]
-            random_id = random.randint(0, len(sent_list)-1)
-            random_response = sent_list[random_id]
+            with open('tfidf.json', 'r') as json_file:
+                pred_json = json.load(json_file)
+            tfidf_response = pred_json['Pred_answer'][q_id].split(".")[0].strip() + "."
+        
+            with open('d2v.json', 'r') as json_file:
+                pred_json = json.load(json_file)
+            doc2vec_response = pred_json['Pred_answer'][q_id].split(".")[0].strip() + "."
 
-            bert_model_dir = "/home/oluwadolapo/Experiments/bertQAnswerability/bert/model_save"
-            model = AnswerabilityModel(bert_model_dir)
-            pred_list = []
-            for idx, sent in enumerate(sent_list):
-                bert_question_context = "[CLS] "+question_text+" [SEP] "+sent
-                pred, prob = model.predict(bert_question_context)
-                pred_dict = {'id':idx, 'pred':pred, 'prob':prob}
-                pred_list.append(pred_dict)
-            
-            best_positive_prob = float('-inf')
-            best_positive_idx = None
-            best_negative_prob = float('inf')
-            best_negative_idx = None
+            with open('bert.json', 'r') as json_file:
+                pred_json = json.load(json_file)
+            sbert_response = pred_json['Pred_answer'][q_id].split(".")[0].strip() + "."
 
-            for pred in pred_list:
-                if pred['pred'] == 1:
-                    if pred['prob'] > best_positive_prob:
-                        best_positive_prob = pred['prob']
-                        best_positive_idx = pred['id']
-                elif pred['pred'] == 0:
-                    if pred['prob'] < best_negative_prob:
-                        best_negative_prob = pred['prob']
-                        best_negative_idx = pred['id']
-            
-            if best_positive_idx:
-                bert_response = sent_list[best_positive_idx]
-            else:
-                bert_response = sent_list[best_negative_idx]
+            with open('bart.json', 'r') as json_file:
+                pred_json = json.load(json_file)
+            stbart_response = pred_json['Pred_answer'][q_id].split(".")[0].strip() + "."
 
+            with open('mt_bart.json', 'r') as json_file:
+                pred_json = json.load(json_file)
+            mtbart_response = pred_json['Pred_answer'][q_id].split(".")[0].strip() + "."
+
+            print(str(tfidf_response))
 
             res = {'question': question_text,
                     'reviews': reviews,
-                    'first': first_response,
-                    'random': random_response,
-                    'bert': bert_response,
-                    'bart': bart_response}
+                    'bm25': bm25_response,
+                    'tf-idf': tfidf_response,
+                    'doc2vec': doc2vec_response,
+                    's-bert': sbert_response,
+                    'st-bart': stbart_response,
+                    'mt-bart': mtbart_response}
 
             return flask.jsonify(res)
 
         except KeyError:
             return app.response_class(response=json.dumps("Index out of range. Enter a lower question index"), status=500, mimetype='application/json')
-    
+
     except Exception as error:
         res = str(error)
         return app.response_class(response=json.dumps(res), status=500, mimetype='application/json')
--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,53 @@
-var data = []
-var token = ""
+var data = [];
+var token = "";
 
 jQuery(document).ready(function () {
-    $('#input_question').keyup(function (e) {
-        if (e.which === 13) {
-            $('#btn-process').click()
-        }
-    });
+  $("#input_question").keyup(function (e) {
+    if (e.which === 13) {
+      $("#btn-process").click();
+    }
+  });
 
-    $('#btn-process').on('click', function () {
-        input_question = $('#input_idx').val()
+  $("#btn-process").on("click", function () {
+    input_question = $("#input_idx").val();
 
-        $.ajax({
-            url: '/get_answer',
-            type: "post",
-            contentType: "application/json",
-            dataType: "json",
-            data: JSON.stringify({
-                "input_idx": input_question
-            }),
-            beforeSend: function () {
-                $('.overlay').show()
-                $('#question').val('')
-                $('#text_paragraphs').val('')
-                $('#first').val('')
-                $('#random').val('')
-                $('#bert').val('')
-                $('#bart').val('')
-            },
-            complete: function () {
-                $('.overlay').hide()
-            }
-        }).done(function (jsondata, textStatus, jqXHR) {
-            console.log(jsondata)
-            $('#question').val(jsondata['question'])
-            $('#text_paragraphs').val(jsondata['reviews'])
-            $('#first').val(jsondata['first'])
-            $('#random').val(jsondata['random'])
-            $('#bert').val(jsondata['bert'])
-            $('#bart').val(jsondata['bart'])
-        }).fail(function (jsondata, textStatus, jqXHR) {
-            console.log(jsondata)
-            alert(jsondata['responseText'])
-        });
+    $.ajax({
+      url: "/get_answer",
+      type: "post",
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify({
+        input_idx: input_question,
+      }),
+      beforeSend: function () {
+        $(".overlay").show();
+        $("#question").val("");
+        $("#text_paragraphs").val("");
+        $("#bm25").val("");
+        $("#tfidf").val("");
+        $("#doc2vec").val("");
+        $("#sbert").val("");
+        $("#stbart").val("");
+        $("#mtbart").val("");
+      },
+      complete: function () {
+        $(".overlay").hide();
+      },
     })
-
-
-})
+      .done(function (jsondata, textStatus, jqXHR) {
+        console.log(jsondata);
+        $("#question").val(jsondata["question"]);
+        $("#text_paragraphs").val(jsondata["reviews"]);
+        $("#bm25").val(jsondata["bm25"]);
+        $("#tfidf").val(jsondata["tf-idf"]);
+        $("#doc2vec").val(jsondata["doc2vec"]);
+        $("#sbert").val(jsondata["s-bert"]);
+        $("#stbart").val(jsondata["st-bart"]);
+        $("#mtbart").val(jsondata["mt-bart"]);
+      })
+      .fail(function (jsondata, textStatus, jqXHR) {
+        console.log(jsondata);
+        alert(jsondata["responseText"]);
+      });
+  });
+});
